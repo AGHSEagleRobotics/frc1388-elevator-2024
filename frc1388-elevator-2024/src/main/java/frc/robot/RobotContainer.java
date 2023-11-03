7758d62ab49e6ccc40b63b17af6652573d222703
Jose <joseflores61609@gmail.com>
2023-11-02 20:49:44 -0700

    uncommented line

--- frc1388-elevator-2024/src/main/java/frc/robot/RobotContainer.java
+++ frc1388-elevator-2024/src/main/java/frc/robot/RobotContainer.java
@@ -21,7 +21,7 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  //private final ElevatorSubsystem m_elevatorSubsystem = new ElevatorSubsystem();
+  private final ElevatorSubsystem m_elevatorSubsystem = new ElevatorSubsystem();
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
   private final XboxController m_driverController =  new XboxController(0);
